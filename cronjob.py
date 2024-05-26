653c7178e69aec819334e58079f1ce66d84452a8
Filip Jakovljevic <fillix96@gmail.com>
2024-05-26 18:08:22 +0200

    fix: cron job not finding data/

--- cronjob.py
+++ cronjob.py
@@ -3,12 +3,18 @@ import pandas as pd
 import os
 
 def update_csv():
-    filename = 'data/test.csv'
+    directory = 'data'
+    filename = os.path.join(directory, 'runs.csv')
+    
+    # create directory if it doesnt exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     df = pd.DataFrame([{'cronjob_run': datetime.now()}])
     
     if not os.path.isfile(filename):
         df.to_csv(filename, index=False)
-        
+
     else:
         df.to_csv(filename, mode='a', header=False, index=False)
 
