400475f4bf6f50ac28b5684ff78ebaf0c0e6607e
Filip Jakovljevic <fillix96@gmail.com>
2024-05-30 20:47:37 +0200

    chore: code cleanup

--- src/app.py
+++ src/app.py
@@ -1,12 +1,26 @@
 import pandas as pd
-from streamlit_patched_ads import st
+import streamlit as st
 from steamlit_filterable_df import StreamlitFilterableDF
 
 st.set_page_config(layout="wide", page_title="Bolje E-Aukcije")
 
+@st.cache_data
+def load_data():
+    df = pd.read_csv("https://raw.githubusercontent.com/FJakovljevic/eaukcije_cronjob/main/data/EAukcija_dump.csv")
+    df['Datum'] = pd.to_datetime(df['Datum'])
+    return df
+
+def get_auctions_df():
+    df = load_data()
+
+    # clearing cache when its passed the day
+    if df['Datum'].max().date() < datetime.today().date():
+        st.cache_data.clear()
+ 
+    return load_data()
+
 st.header("Bolja E-Aukcija", divider="green")
-st.markdown(
-    """
+st.markdown("""
 Kako sajt [eaukcija.sud.rs](https://eaukcija.sud.rs/#/) ne nudi mogućnosti filtriranja aukcija i veoma je nepregledan za korišćenje, 
 razvijen je ovaj sajt koji upravo nudi mogućnosti filtriranja i pregleda svih budućih aukcija. 
 Moguće je filtrirati i sortirati po `Tipu`, `Kategoriji`, `Opstini`, `Početnoj ceni` i `datumu`, 
@@ -14,15 +28,7 @@ a za svaku aukciju prisutan je link ka originalu na sajtu ministarstva.
 **Dobrodošli na Bolju E-Aukciju!**
 
 Aukcije:
-"""
-)
-
-
-@st.cache_data
-def load_data():
-    url = "https://raw.githubusercontent.com/FJakovljevic/eaukcije_cronjob/main/data/EAukcija_dump.csv"
-    return pd.read_csv(url)
-
+""")
 
 columns_to_filter = ["Tip", "Kategorija", "Opstina", "Ponovljena", "Pocetna Cena", "Datum"]
 columns_formatting = {
@@ -33,8 +39,5 @@ columns_formatting = {
     "Procenjena Vrednost": st.column_config.NumberColumn("Procenjena Vrednost"),
 }
 
-dynamic_filters = StreamlitFilterableDF(df=load_data(), columns_to_filter=columns_to_filter)
+dynamic_filters = StreamlitFilterableDF(df=get_auctions_df(), columns_to_filter=columns_to_filter)
 dynamic_filters.display(use_container_width=True, column_config=columns_formatting, hide_index=True, height=500)
-
-
-st.html("""<iframe src="adsens.html" width="100%" height="200" frameborder="0" scrolling="no"></iframe>""")
